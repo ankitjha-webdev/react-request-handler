e5e014dca8d68ccc34d61c0225929bd5eca284a1
ANKIT KUMAR <190101120091@cutm.ac.in>
2023-11-25 10:02:09 +0000

    initial commit

---
+++ src/pages/users.tsx
@@ -0,0 +1,19 @@
+import { getUsers } from "../components/users";
+
+const Users = async () => {
+    const usersResponse = await getUsers({ limit: 10, page: 1 })
+    if (usersResponse.code === "error") {
+        return <div> {usersResponse.error.message} </div>
+    }
+
+    return <>
+        {usersResponse.data.map((user) => (
+            <div key={user.id}>
+                {user.name}
+            </div>
+        ))}
+    </>
+
+}
+
+export default Users;
